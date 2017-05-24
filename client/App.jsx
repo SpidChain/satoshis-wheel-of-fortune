35743f600b24453d25d0ba52c63039b1ae87eedc
Nicola Squartini <tensor5@gmail.com>
2017-05-24 21:11:11 +0900

    Clean up

--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, hashHistory } from 'react-router-dom'
-
+import {BrowserRouter, Route, hashHistory} from 'react-router-dom'
+import {Meteor} from 'meteor/meteor'
 import Tether from 'tether'
+
+import Main from '/imports/client/Main'
+import {initWheel} from '/imports/client/wheel'
+
 // Export Bootstrap
 window.Tether = Tether
 require('bootstrap')
 
-//import '/imports/client/styles/'
-import Main from '/imports/client/Main'
-import {initWheel} from '/imports/client/wheel'
-
-
 Meteor.startup(() => {
   ReactDOM.render(
     <BrowserRouter history={hashHistory}>
-      <Route path="/" component={Main}/>
+      <Route path='/' component={Main} />
     </BrowserRouter>,
     document.getElementById('app'))
   initWheel()
 })
-
