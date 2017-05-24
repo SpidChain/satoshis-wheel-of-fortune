d4494799da5a1c803f2d941f64ab864d93bc3061
Nicola Squartini <tensor5@gmail.com>
2017-05-24 21:50:55 +0900

    Remove useless imports

--- client/App.jsx
+++ client/App.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Route, hashHistory} from 'react-router-dom'
 import {Meteor} from 'meteor/meteor'
-import Tether from 'tether'
 
 import Main from '/imports/client/Main'
 import {initWheel} from '/imports/client/wheel'
-
-// Export Bootstrap
-window.Tether = Tether
-require('bootstrap')
 
 Meteor.startup(() => {
   ReactDOM.render(
