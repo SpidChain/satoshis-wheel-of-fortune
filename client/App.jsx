64db70c47a61acbef60f9ca87893c517fe5cd757
Federico Squartini <federico.squartini@gmail.com>
2017-05-24 16:53:07 +0200

    Style the navbar with bootstrap

--- client/App.jsx
+++ client/App.jsx
@@ -12,14 +12,24 @@ Meteor.startup(() => {
   ReactDOM.render(
     <BrowserRouter history={browserHistory}>
       <div>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/participants">Participants</Link></li>
-        <li><Link to="/setup">Setup</Link></li>
-        <Route exact='true' path="/" component={Main}/>
-        <Route path="/setup" component={Setup}/>
-        <Route path="/participants" component={ParticipantsContainer}/>
-      </div>
-    </BrowserRouter>,
-    document.getElementById('app'))
-  initWheel()
-})
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand" href="#">Spidchain</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active"><Link className="nav-link" to="/">Home</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/participants">Participants</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/setup">Setup</Link></li>
+            </ul>
+          </div>
+          </nav>
+          <Route exact='true' path="/" component={Main}/>
+          <Route path="/setup" component={Setup}/>
+          <Route path="/participants" component={ParticipantsContainer}/>
+        </div>
+      </BrowserRouter>,
+      document.getElementById('app'))
+  //initWheel()
+      })
