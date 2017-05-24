4113f51376189d7df8afcb89a691c22e90dcccd2
Federico Squartini <federico.squartini@gmail.com>
2017-05-24 17:40:40 +0200

    Add method to set blocknumber in collection

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,13 @@
 Meteor.methods({
   addParticipants: (participants) => {
-    console.log(participants, typeof participants)
     ParticipantsC.remove({})
     participants
       .split('\n')
       .forEach((e) => ParticipantsC.insert({name: e}))
+  },
+
+  setBlocknumber: (blocknumber) => {
+    Blocknumber.remove({})
+    Blocknumber.insert({blocknumber})
   }
 })
