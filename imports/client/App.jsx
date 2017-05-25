17b96329b11f4dfba6e250eda806077e5bc2769d
Federico Squartini <federico.squartini@gmail.com>
2017-05-25 22:20:53 +0200

    Add fork me on github ribbon

--- imports/client/App.jsx
+++ imports/client/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, browserHistory} from 'react-router-dom'
+import GitHubForkRibbon from 'react-github-fork-ribbon'
 
 import NavBar from '/imports/client/NavBar'
 import Main from '/imports/client/Main'
@@ -9,12 +10,20 @@ import Participants from '/imports/client/Participants'
 const App = ({loading, participants, blockNumber, userId}) => {
   return (<BrowserRouter history={browserHistory}>
     <div>
+      <GitHubForkRibbon
+        href="https://github.com/SpidChain/satoshis-wheel-of-fortune"
+        target="_blank"
+        position="right">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+
       <NavBar userId={userId} />
+
       <Route exact path='/' component={props => <Main
-        loading={loading}
-        participants={participants}
-        blockNumber={blockNumber}
-        userId={userId} />} />
+          loading={loading}
+          participants={participants}
+          blockNumber={blockNumber}
+          userId={userId} />} />
 
       <Route exact path='/setup' component={(props) => <Setup userId={userId} />} />
 
