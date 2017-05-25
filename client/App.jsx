fb102cffbb307f6e7f1fddda1001874ab22dc7e1
Nicola Squartini <tensor5@gmail.com>
2017-05-25 17:20:22 +0900

    Use reactstrap components for navbar

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Collapse, Nav, NavItem, Navbar, NavbarBrand, NavbarToggler} from 'reactstrap'
 import {Meteor} from 'meteor/meteor'
 import {BrowserRouter, Route, browserHistory, Link} from 'react-router-dom'
 
@@ -11,19 +12,17 @@ Meteor.startup(() => {
   ReactDOM.render(
     <BrowserRouter history={browserHistory}>
       <div>
-        <nav className='navbar navbar-toggleable-md navbar-light bg-faded'>
-          <button className='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon' />
-          </button>
-          <a className='navbar-brand' href='#'>Spidchain</a>
-          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul className='navbar-nav mr-auto'>
-              <li className='nav-item active'><Link className='nav-link' to='/'>Home</Link></li>
-              <li className='nav-item'><Link className='nav-link' to='/participants'>Participants</Link></li>
-              <li className='nav-item'><Link className='nav-link' to='/setup'>Setup</Link></li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar toggleable='md' light color='faded'>
+          <NavbarToggler right data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation' />
+          <NavbarBrand>Spidchain</NavbarBrand>
+          <Collapse navbar id='navbarSupportedContent'>
+            <Nav navbar className='mr-auto'>
+              <NavItem className='active'><Link className='nav-link' to='/'>Home</Link></NavItem>
+              <NavItem><Link className='nav-link' to='/participants'>Participants</Link></NavItem>
+              <NavItem><Link className='nav-link' to='/setup'>Setup</Link></NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
         <Route exact path='/' component={MainContainer} />
         <Route path='/setup' component={Setup} />
         <Route path='/participants' component={ParticipantsContainer} />
