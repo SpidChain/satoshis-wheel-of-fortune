eb8e2bbfc6d5faa464cb1ecbb236604dbef988c4
Federico Squartini <federico.squartini@gmail.com>
2017-05-25 16:45:17 +0200

    Do not populate users list at startup

--- server/publications/participants.js
+++ server/publications/participants.js
@@ -2,9 +2,6 @@ import {Meteor} from 'meteor/meteor'
 
 import ParticipantsC from '/imports/collections/partecipants'
 
-ParticipantsC.insert({name: 'ciro'})
-ParticipantsC.insert({name: 'lorenzo'})
-ParticipantsC.insert({name: 'duccio'})
 Meteor.publish('participants', () => {
   /*
   return Lists.find({
