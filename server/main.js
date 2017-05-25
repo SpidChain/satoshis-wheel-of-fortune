ded0cf69338d2290338e2a02da8cf10cbddf617f
Federico Squartini <federico.squartini@gmail.com>
2017-05-25 16:56:54 +0200

    Add Meteor.settings

--- server/main.js
+++ server/main.js
@@ -1,8 +1,10 @@
 import { Accounts } from 'meteor/accounts-base'
 
 const createAdmin = () => {
+  username = Meteor.settings.username
+  password = Meteor.settings.password
   if(! Meteor.users || Meteor.users.find().count() === 0) {
-    Accounts.createUser({username: 'spidchain', password: 'xxxyyyzzz'})
+    Accounts.createUser({username, password})
   }
 }
 
